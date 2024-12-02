6565b42c03e502f3140f21273d355786d5ce0012
Julien Capellari <julien.capellari@gmail.com>
2024-12-02 12:01:35 +0100

    feat: resolve day-02 (part 2)

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 
 macro_rules! read_lines {
     ($file:literal) => {
@@ -9,26 +10,39 @@ macro_rules! read_lines {
     };
 }
 
+fn is_safe(levels: &Vec<i32>) -> bool {
+    let spans = levels.windows(2)
+        .map(|w| w[1] - w[0])
+        .collect::<Vec<_>>();
+
+    let same_sign = spans[1..].iter()
+        .all(|n| n * spans[0] > 0);
+
+    let below_3 = spans.iter().all(|n| n.abs() <= 3);
+
+    same_sign && below_3
+}
+
 fn main() {
     let mut safe_cnt = 0;
+    let mut dampener_safe_cnt = 0;
 
     for line in read_lines!("day-02/input.txt") {
-        let spans = line.split_whitespace()
+        let levels = line.split_whitespace()
             .map(|n| n.parse().unwrap())
-            .collect::<Vec<i32>>()
-            .windows(2)
-            .map(|w| w[1] - w[0])
-            .collect::<Vec<_>>();
+            .collect::<Vec<i32>>();
 
-        let same_sign = spans[1..].iter()
-            .all(|n| n * spans[0] > 0);
-
-        let below_3 = spans.iter().all(|n| n.abs() <= 3);
-
-        if same_sign && below_3 {
+        if is_safe(&levels) {
             safe_cnt += 1;
+        }
+
+        if levels.iter()
+            .combinations(levels.len() - 1)
+            .any(|levels| is_safe(&levels.iter().copied().copied().collect())) {
+            dampener_safe_cnt += 1;
         }
     }
 
     println!("part 01: {}", safe_cnt);
+    println!("part 02: {}", dampener_safe_cnt);
 }
