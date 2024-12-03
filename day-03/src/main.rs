0c609c2f14135f6c7369c6b9c8077bc75fda23b9
Julien Capellari <julien.capellari@gmail.com>
2024-12-03 22:43:01 +0100

    feat: resolve day-03 (part 2)

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -11,17 +11,38 @@ macro_rules! read_lines {
 }
 
 fn main() {
-    let re = Regex::new(r"mul\((?<a>[0-9]+),(?<b>[0-9]+)\)").unwrap();
-    let mut sum = 0;
+    let re = Regex::new(r"((?<op>mul|do|don't)\(((?<a>[0-9]+),(?<b>[0-9]+))?\))").unwrap();
+    let mut enabled = true;
+    let mut part01 = 0;
+    let mut part02 = 0;
 
     for line in read_lines!("day-03/input.txt") {
-        for op in re.captures_iter(&line) {
-            let a: i32 = op.name("a").unwrap().as_str().parse().unwrap();
-            let b: i32 = op.name("b").unwrap().as_str().parse().unwrap();
+        for expr in re.captures_iter(&line) {
+            match expr.name("op").unwrap().as_str() {
+                "mul" => {
+                    if let Some(a) = expr.name("a") {
+                        let a = a.as_str().parse::<i32>().unwrap();
+                        let b = expr.name("b").unwrap().as_str().parse::<i32>().unwrap();
+                        let r = a * b;
 
-            sum += a * b;
+                        part01 += r;
+
+                        if enabled {
+                            part02 += r;
+                        }
+                    }
+                }
+                "do" => {
+                    enabled = true;
+                }
+                "don't" => {
+                    enabled = false;
+                }
+                _ => unreachable!(),
+            }
         }
     }
 
-    println!("part 01: {sum}");
+    println!("part 01: {part01}");
+    println!("part 02: {part02}");
 }
