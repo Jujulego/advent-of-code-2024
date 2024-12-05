a832b36ecb4778f577fa0a5ae2b5c57ef37bb65e
Julien Capellari <julien.capellari@gmail.com>
2024-12-05 20:25:40 +0100

    feat: resolve day-05 (part 2)

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::{HashMap, HashSet};
+use std::mem::swap;
 
 macro_rules! read_lines {
     ($file:literal) => {
@@ -10,12 +11,34 @@ macro_rules! read_lines {
     };
 }
 
+fn check_update(update: &[i32], rules: &HashMap<i32, HashSet<i32>>) -> Option<(usize, usize)> {
+    let mut previous = HashSet::new();
+
+    for (idx, page) in update.iter().enumerate() {
+        if let Some(next) = rules.get(page) {
+            let common = previous.intersection(next)
+                .map(|p| update[..idx].iter().position(|r| r == p).unwrap())
+                .min();
+
+            if let Some(index) = common {
+                return Some((idx, index)); // <= returns index pair to invert
+            }
+        }
+
+        previous.insert(*page);
+    }
+
+    None
+}
+
 fn main() {
     let mut rules: HashMap<i32, HashSet<i32>> = HashMap::new();
     let mut rules_loaded = false;
     let mut part01 = 0;
+    let mut part02 = 0;
+    let mut moves = 0;
 
-    'lines: for line in read_lines!("day-05/input.txt") {
+    for line in read_lines!("day-05/input.txt") {
         if line.is_empty() {
             rules_loaded = true;
             continue;
@@ -34,23 +57,30 @@ fn main() {
                 .insert(y);
         } else {
             // Parse updates
-            let mut pages = HashSet::new();
-            let mut update = Vec::new();
-            
-            for page in line.split(',').map(|p| p.parse::<i32>().unwrap()) {
-                if let Some(nexts) = rules.get(&page) {
-                    if pages.intersection(nexts).count() > 0 { // <= previous pages includes next ones
-                        continue 'lines; // <= invalid update go to next line
-                    }
+            let mut update: Vec<i32> = line.split(',').map(|p| p.parse().unwrap()).collect();
+            let mut correct = true;
+
+            while let Some((to_move_idx, before_idx)) = check_update(&update, &rules) {
+                assert!(to_move_idx > before_idx);
+                correct = false;
+
+                let mut tmp = update[to_move_idx];
+
+                for page in &mut update[before_idx..=to_move_idx] {
+                    swap(page, &mut tmp);
                 }
-                
-                pages.insert(page);
-                update.push(page);
+
+                moves += 1;
             }
-            
-            part01 += update[update.len() / 2];
+
+            if correct {
+                part01 += update[update.len() / 2];
+            } else {
+                part02 += update[update.len() / 2];
+            }
         }
     }
 
     println!("part 01: {part01}");
+    println!("part 02: {part02} ({moves})");
 }
