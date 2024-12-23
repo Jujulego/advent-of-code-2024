f647a77ec3b6300680a69b2bc9ceaabd9d16ba34
Julien Capellari <julien.capellari@gmail.com>
2024-12-23 14:56:22 +0100

    feat: resolve day-23 (part 02)

--- day-23/src/main.rs
+++ day-23/src/main.rs
@@ -1,4 +1,5 @@
-use std::collections::{HashMap, HashSet};
+use itertools::Itertools;
+use std::collections::{BTreeSet, HashMap, HashSet, VecDeque};
 use std::time::Instant;
 
 macro_rules! read_lines {
@@ -17,35 +18,62 @@ fn main() {
         let a = parts.next().unwrap().to_string();
         let b = parts.next().unwrap().to_string();
 
-        links.entry(a.clone()).or_insert_with(HashSet::new)
+        links.entry(a.clone()).or_insert_with(BTreeSet::new)
             .insert(b.clone());
 
-        links.entry(b).or_insert_with(HashSet::new)
+        links.entry(b).or_insert_with(BTreeSet::new)
             .insert(a);
     }
-    
+
     // Part 01
     let now = Instant::now();
     let pairs = links.iter()
         .flat_map(|(a, to)| to.iter().map(move |b| (a, b)));
-   
+
     let mut groups = HashSet::new();
-    
+
     for (a, b) in pairs {
         let to_a = links.get(a).unwrap();
         let to_b = links.get(b).unwrap();
-        
+
         for c in to_a.intersection(to_b) {
-            let mut group = [a, b, c];
-            group.sort();
-            
+            let group = BTreeSet::from([a, b, c]);
             groups.insert(group);
         }
     }
-    
+
     let part01 = groups.iter()
         .filter(|grp| grp.iter().any(|c| c.starts_with("t")))
         .count();
-    
+
     println!("part 01: {} ({:.2?})", part01, now.elapsed());
+
+    // Part 02
+    let now = Instant::now();
+    let mut marks = HashSet::new();
+    let mut queue = VecDeque::from_iter(links.keys().cloned());
+    let mut groups = Vec::new();
+
+    while let Some(computer) = queue.pop_back() {
+        if !marks.insert(computer.clone()) {
+            continue;
+        }
+
+        let linked = links.get(&computer).unwrap();
+        queue.extend(linked.iter().cloned()); // Insert all next computers
+
+        for grp in &mut groups {
+            if linked.is_superset(&grp) {
+                grp.insert(computer.clone());
+            }
+        }
+
+        groups.push(BTreeSet::from([computer]));
+    }
+
+    let party = groups.iter()
+        .max_by(|a, b| a.len().cmp(&b.len()))
+        .unwrap();
+
+    println!("part 02: {} ({:.2?})", party.iter().join(","), now.elapsed());
 }
